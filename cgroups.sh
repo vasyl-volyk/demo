54b658647b3663c834021bb2b4da7a6d1b118f2c
vasyl-volyk <vasyl.volyk@outlook.com>
2025-05-16 04:00:20 +0000

    add files

--- cgroups.sh
+++ cgroups.sh
@@ -10,7 +10,8 @@ sudo apt install cgroup-tools stress
 
 sudo mkdir /sys/fs/cgroup/unified
 sudo mount -t cgroup2 none /sys/fs/cgroup/unified
-sudo cgcreate -g cpuset, memory:unified/demo
+sudo cgcreate -g cpuset, memory:unified/demo 
+# WORKS --> sudo mkdir /sys/fs/cgroup/demo
 
 # Check the cgroup
 sudo cgget -g cpuset unified/demo
@@ -20,3 +21,14 @@ sudo cgget -g memory unified/demo
 sudo cgset -r memory.max=100M unified/demo
 sudo cgexec -g cpu:unified/demo htop
 sudo cgset -r memory.max=100K unified/demo
+
+# Run the stress tool
+htop
+sudo cgexec -g cpu:unified/demo stress --cpu 2 &
+sudo cgset -r cpuset.cpus=0 unified/demo
+
+# Freeze the cgroup
+echo 1 | sudo tee /sys/fs/cgroup/unified/demo/cgroup.freeze
+
+# Unfreeze the cgroup
+echo 0 | sudo tee /sys/fs/cgroup/unified/demo/cgroup.freeze
